a3881c82b3a2b08da05c720ad6b2e6dcdabb6088
Kevin PESET <kevin.peset@gmail.com>
2025-04-04 11:38:20 +0200

    Ajout des liens et suppression du css

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Navbar() {
-	return <nav>Navbar</nav>;
+	return (
+		<nav>
+			<Link href="/">Accueil</Link>
+			<Link href="/refuges">Refuges</Link>
+		</nav>
+	);
 }
---
+++ pages/refuges.tsx
@@ -0,0 +1,7 @@
+export default function Refuges() {
+	return (
+		<>
+			<h2>Les reguges</h2>
+		</>
+	);
+}
